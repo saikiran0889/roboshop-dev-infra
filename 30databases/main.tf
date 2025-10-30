3c830db163c4fe79e29ecdae90c1a92f954e975b
saikiran0889 <saikiranbuddha1234@gmail.com>
2025-10-30 21:41:49 +0530

    terraform

--- 30databases/main.tf
+++ 30databases/main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "mongodb" {
 
 resource "terraform_data" "mongodb" {
   triggers_replace = [
-    aws_instance.i-09e1307fc2cc33dfe
+    aws_instance.mongodb.id
   ]
 connection {
   type = "ssh"
