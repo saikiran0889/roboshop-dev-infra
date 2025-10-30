9b8c0de611c763cf8b64da9652fb7e8ebb91ff56
saikiran0889 <saikiranbuddha1234@gmail.com>
2025-10-30 17:35:32 +0530

    tf

--- 40SG-Rules/data.tf
+++ 40SG-Rules/data.tf
@@ -6,4 +6,6 @@ data "aws_ssm_parameter" "bastion_alb_sg_id" {
   name = "/${var.project}/${var.environment}/bastion_sg_id"
 }
 
-
+data "aws_ssm_parameter" "mongodb_sg_id" {
+  name = "/${var.project}/${var.environment}/mongodb_sg_id"
+}
