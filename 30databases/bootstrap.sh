f487660d700402e1f115fe9bff743a58e95e5d90
saikiran0889 <saikiranbuddha1234@gmail.com>
2025-10-31 15:39:20 +0530

    terraform

--- 30databases/bootstrap.sh
+++ 30databases/bootstrap.sh
@@ -2,3 +2,7 @@
 
 dnf install ansible -y
 ansible-pull -u https://github.com/saikiran0889/ansible-robodhop-roles-installpackages-dev-infra.git -e component=mongodb main.yaml
+
+git clone ansible-playbook
+cd ansible-playbook
+ansible-playbook -i inventory main.yaml
