1dd151500b7afdd7a1280202dcbf9c8d7903e56b
saikiran0889 <saikiranbuddha1234@gmail.com>
2025-11-02 10:46:18 +0530

    terraform

--- 30databases/main.tf
+++ 30databases/main.tf
@@ -120,3 +120,44 @@ provisioner "file" {
     ]
   }
 }
+
+
+
+resource "aws_instance" "mysql" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.mysql_sg_id]
+  subnet_id = local.database_subnet_ids
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}-mysql"
+    }
+
+
+  )
+} 
+
+resource "terraform_data" "mysql" {
+  triggers_replace = [
+    aws_instance.mysql.id
+  ]
+connection {
+  type = "ssh"
+  user = "ec2-user"
+  password = "DevOps321"
+  host = aws_instance.mysql.private_ip  
+}
+provisioner "file" {
+  source = "bootstrap.sh"  
+  destination = "/tmp/bootstrap.sh"
+}
+
+ provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        # "sudo sh /tmp/bootstrap.sh"
+        "sudo sh /tmp/bootstrap.sh mysql"
+    ]
+  }
+}
