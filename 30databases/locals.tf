7e50140cbb03ef581e82ffff8b98e2e0b19f500b
saikiran0889 <saikiranbuddha1234@gmail.com>
2025-11-02 10:29:35 +0530

    terraform

--- 30databases/locals.tf
+++ 30databases/locals.tf
@@ -2,6 +2,8 @@ locals {
   ami_id = data.aws_ami.awsdevops.id
   mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
   redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
+  rabbitmq_sg_id = data.aws_ssm_parameter.rabbitmq_sg_id.value
+  mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
   database_subnet_ids = split("," , data.aws_ssm_parameter.database_subnet_ids.value)[0]
 
   common_tags = {
